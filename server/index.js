f97fab73b82e4da28671bee0e544844527e7caec
Sam-McCathie <sammccathie2@gmail.com>
2021-08-10 11:34:23 +1200

    Create backend & routes

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db.js");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+/* ROUTES */
+// create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const {description} = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description] //description is the value of req.body as above.
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// get a todo - get data based on PK ID
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+    res.json("Job done");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+// delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const {id} = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server runninng on port ${port}.`);
+});
